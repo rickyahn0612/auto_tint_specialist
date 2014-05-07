b5e0e03c624fe0bbc51d7d4ceff2828dae50c9d9
rickyahn0612 <rickyahn0612@yahoo.com>
2014-05-07 11:39:31 -0700

    added in get method for mailer.rn

--- mailer.rb
+++ mailer.rb
@@ -2,9 +2,21 @@ require 'sinatra'
 require 'pony'
 
 puts 'supmotherfucker'
-post '/contact' do
-  Pony.mail(:to => 'rickyahn0612@yahoo.com', :from => "someone@onehouse.net", :subject => "art inquiry from #{name}", :body => "Sup")
 
-  haml :contact
+get '/contact' do
+    haml :contact
+end
+
+
+post '/contact' do
+post '/contact' do
+        name = params[:name]
+        mail = params[:mail]
+        body = params[:body]
+
+        Pony.mail(:to => 'rickyahn0612@yahoo.com', :from => "#{mail}", :subject => "art inquiry from #{name}", :body => "#{body}")
+
+        haml :contact
+    end
 end
 
